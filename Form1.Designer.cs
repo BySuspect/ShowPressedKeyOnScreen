0c1cce13ddc1aefac5e8aeba3c6f6083878bdaf3
BySuspect <omerbayraktar43@gmail.com>
2023-02-23 15:37:40 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ShowPressedKeyOnScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblKeyPressed = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblKeyPressed
+            // 
+            this.lblKeyPressed.BackColor = System.Drawing.SystemColors.Control;
+            this.lblKeyPressed.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblKeyPressed.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold);
+            this.lblKeyPressed.Location = new System.Drawing.Point(0, 0);
+            this.lblKeyPressed.Name = "lblKeyPressed";
+            this.lblKeyPressed.Size = new System.Drawing.Size(465, 135);
+            this.lblKeyPressed.TabIndex = 0;
+            this.lblKeyPressed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblKeyPressed.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMouseDown);
+            this.lblKeyPressed.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormMouseMove);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 80);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(465, 135);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.lblKeyPressed);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Keyboard Key Listener & Shower";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Green;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormMouseMove);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKeyPressed;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowPressedKeyOnScreen
+{
+    public partial class Form1 : Form
+    {
+
+        bool PressingCTRL = false;
+        bool PressingALT = false;
+        bool PressingSHIFT = false;
+        bool PressingKey = false;
+        List<string> PressedKeys = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Mouse move codes
+        private Point _mouseLoc;
+        private void FormMouseDown(object sender, MouseEventArgs e)
+        {
+            _mouseLoc = e.Location;
+        }
+        private void FormMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int dx = e.Location.X - _mouseLoc.X;
+                int dy = e.Location.Y - _mouseLoc.Y;
+                this.Location = new Point(this.Location.X + dx, this.Location.Y + dy);
+            }
+        }
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Point loc = new Point(Screen.AllScreens[0].WorkingArea.Width - this.Width, Screen.AllScreens[0].WorkingArea.Height - (this.Height + 20));
+            this.Location = loc;
+
+            // Watch for keyboard  activity
+            KeyboardListener.s_KeyEventHandler += new EventHandler(KeyboardListener_s_KeyEventHandler);
+        }
+
+        private void KeyboardListener_s_KeyEventHandler(object sender, EventArgs e)
+        {
+            KeyboardListener.UniversalKeyEventArgs eventArgs = (KeyboardListener.UniversalKeyEventArgs)e;
+            Debug.WriteLine(eventArgs.m_Msg);
+            if (eventArgs.m_Msg == 256) // key down and while press
+            {
+                Debug.WriteLine($"EventMsg: KeyDown  KeyValue: {eventArgs.KeyValue} KeyData: {eventArgs.KeyData} KeyCode: {eventArgs.KeyCode}");
+                if (eventArgs.KeyData == Keys.ControlKey)
+                {
+                    PressingCTRL = true;
+                }
+                else if (eventArgs.KeyData == Keys.Menu)
+                {
+                    PressingALT = true;
+                }
+                else if (eventArgs.KeyData == Keys.ShiftKey)
+                {
+                    PressingSHIFT = true;
+                }
+                PressedKeys.Add(eventArgs.KeyData.ToString());
+                PressedKeys = RemoveDuplicates(PressedKeys);
+                lblKeyPressed.Text = " ";
+                PressingKey = true;
+                foreach (var item in PressedKeys)
+                {
+                    lblKeyPressed.Text += KeysToString(item) + " ";
+                }
+            }
+            else
+            {
+                PressedKeys.Add(eventArgs.KeyData.ToString());
+                PressedKeys = RemoveDuplicates(PressedKeys);
+                lblKeyPressed.Text = " ";
+                PressingKey = true;
+                foreach (var item in PressedKeys)
+                {
+                    lblKeyPressed.Text += KeysToString(item) + " ";
+                }
+            }
+
+            if (eventArgs.m_Msg == 257) // key up
+            {
+                Debug.WriteLine($"EventMsg: KeyUp  KeyValue: {eventArgs.KeyValue} KeyData: {eventArgs.KeyData} KeyCode: {eventArgs.KeyCode}");
+                if (eventArgs.KeyData == Keys.ControlKey)
+                {
+                    PressingCTRL = false;
+                }
+                else if (eventArgs.KeyData == Keys.Menu)
+                {
+                    PressingALT = false;
+                }
+                else if (eventArgs.KeyData == Keys.ShiftKey)
+                {
+                    PressingSHIFT = false;
+                }
+                PressedKeys = RemoveDuplicates(PressedKeys);
+                PressedKeys.Remove(eventArgs.KeyData.ToString());
+                lblKeyPressed.Text = " ";
+                foreach (var item in PressedKeys)
+                {
+                    lblKeyPressed.Text += KeysToString(item) + " ";
+                }
+            }
+        }
+        public List<string> RemoveDuplicates(List<string> list)
+        {
+            List<string> result = new List<string>();
+            foreach (string item in list)
+            {
+                if (!result.Contains(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+        public string KeysToString(string keyData)
+        {
+            KeysConverter converter = new KeysConverter();
+            Keys keysdata = (Keys)converter.ConvertFromString(keyData);
+            Keys key = keysdata & Keys.KeyCode;
+
+            if (key >= Keys.D0 && key <= Keys.D9)
+            {
+                return (key - Keys.D0).ToString();
+            }
+            else if (key >= Keys.NumPad0 && key <= Keys.NumPad9)
+            {
+                return (key - Keys.NumPad0).ToString();
+            }
+            else
+            {
+                switch (key)
+                {
+                    case Keys.Oemtilde:
+                        return "`";
+                    case Keys.OemMinus:
+                        return "-";
+                    case Keys.Oemplus:
+                        return "=";
+                    case Keys.OemOpenBrackets:
+                        return "[";
+                    case Keys.OemCloseBrackets:
+                        return "]";
+                    case Keys.OemPipe:
+                        return "\\";
+                    case Keys.OemSemicolon:
+                        return ";";
+                    case Keys.OemQuotes:
+                        return "'";
+                    case Keys.Oemcomma:
+                        return ",";
+                    case Keys.OemPeriod:
+                        return ".";
+                    case Keys.OemQuestion:
+                        return "/";
+                    default:
+                        return key.ToString();
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PressedKeys.Clear();
+            lblKeyPressed.Text = " ";
+        }
+    }
+}
